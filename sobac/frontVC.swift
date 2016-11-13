4ee97bfe1b150029e243076d5e162ea2c36231ba
Daniel Porter <danielporter@8cg5242ym9.gcc.edu>
2016-11-13 15:19:18 -0500

    Initial Commit

---
+++ sobac/frontVC.swift
@@ -0,0 +1,35 @@
+//
+//  frontVC.swift
+//  sobac
+//
+//  Created by Daniel Porter on 11/11/16.
+//  Copyright © 2016 Daniel Porter. All rights reserved.
+//
+
+import UIKit
+
+class frontVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var progressCircle: CircleProgressView!
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
