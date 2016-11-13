33f1234a84ecdb7a12d21fb7cea35bb50f7e995f
Daniel Porter <danielporter@8cg5242ym9.gcc.edu>
2016-11-13 17:59:31 -0500

    Added all files that we will need for this project.

---
+++ sobac/FriendCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendCell.swift
+//  sobac
+//
+//  Created by Daniel Porter on 11/13/16.
+//  Copyright © 2016 Daniel Porter. All rights reserved.
+//
+
+import UIKit
+
+class FriendCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ sobac/newBACButton.swift
@@ -0,0 +1,21 @@
+//
+//  newBACButton.swift
+//  sobac
+//
+//  Created by Daniel Porter on 11/13/16.
+//  Copyright © 2016 Daniel Porter. All rights reserved.
+//
+
+import UIKit
+
+class newBACButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
