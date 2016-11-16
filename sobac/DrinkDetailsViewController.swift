6361cc14c32b94bcb273df484aee62a13cdbf5a8
Nate Henry <natehenry@Nates-MacBook-Air-2.local>
2016-11-15 22:03:22 -0500

    Final additions to storyboard (hopefully)

--- sobac/DrinkDetailsViewController.swift
+++ sobac/DrinkDetailsViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class DrinkDetailsViewController: UIViewController {
 
+    @IBOutlet weak var drinkWebView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
